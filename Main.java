71b6d89a7dced76d89506a16841effe8041286f8
xFru1T <max.shibaefff@gmail.com>
2025-05-18 20:22:55 +0300

    Result

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StringRepeat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Print number: ");
+        float a = scanner.nextFloat() *-1;
+        System.out.print(a);
+    }
+}
